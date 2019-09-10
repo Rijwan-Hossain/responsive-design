5e58f5ec272ce28b0513ea3af40213a3b6c90bd4
Rijwan Hossain <rijyanhossain@gmail.com>
2019-09-09 19:36:08 -0700

    font size reduced of dialog box

--- components/shared/DialogBox.js
+++ components/shared/DialogBox.js
@@ -4,12 +4,15 @@ import { UncontrolledAlert } from 'reactstrap';
 function DialogBox() {
   return (
     <UncontrolledAlert color="info" className="px-5 pb-0 mb-0">
-        <p className="px-5"> 
+        <p style={{
+          fontSize: '13px'
+        }} 
+        className="px-5"> 
           This static project is made by ReactJS & NextJS for SSR.
         </p> 
         <p style={{
           fontSize: '13px'
-        }}>
+        }} className="px-5">
           Source Code: https://github.com/Rijwan-Hossain/responsive-design
         </p> 
         <p style={{ 
