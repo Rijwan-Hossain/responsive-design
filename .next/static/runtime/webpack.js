604e7d48fdb028cb240d719fd58b990da903e78c
Rijwan Hossain <rijyanhossain@gmail.com>
2019-09-25 00:15:04 -0700

    edit about me info to collapsable

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "64132b69754222f2b07d";
+/******/ 	var hotCurrentHash = "4d72c422f5300f3dc29f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
